a097d0e4f41754bc2df352f25ebd40ca836e3161
Shivang Pathak <shivang.10688439@ltimindtree.com>
2024-10-03 11:02:12 +0530

    added new excel util file

---
+++ src/main/java/com/qa/opencart/utils/ExcelUtils.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.utils;
+
+public class ExcelUtils {
+
+	public static void main(String[] args) {
+		
+		System.out.println("in this class will add excel utils logic");
+	}
+
+}

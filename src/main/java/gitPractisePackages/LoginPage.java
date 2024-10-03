062b51e9d7456adddddfaf31a668b3b893fd02a7
Shivang Pathak <shivang.10688439@ltimindtree.com>
2024-10-03 11:24:41 +0530

    pages logic added according to the reviewer comment

--- src/main/java/gitPractisePackages/LoginPage.java
+++ src/main/java/gitPractisePackages/LoginPage.java
@@ -3,7 +3,22 @@ package gitPractisePackages;
 public class LoginPage {
 
 	public static void main(String[] args) {
-		
+
+		System.out.println("this is the page logic");
+
+		String username = "Ravi";
+
+		int size = username.length();
+
+		if (size != 0) {
+
+			System.out.println("its correct username");
+		} else {
+
+			System.out.println("user not found");
+
+		}
+
 	}
 
 }
